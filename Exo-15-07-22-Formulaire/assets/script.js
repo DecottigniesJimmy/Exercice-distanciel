542d265e8cf633b4abbc04e80698842ace730aac
DecottigniesJimmy <106061368+DecottigniesJimmy@users.noreply.github.com>
2022-07-15 11:27:57 +0200

    Updt 15/07/22

--- Exo-15-07-22-Formulaire/assets/script.js
+++ Exo-15-07-22-Formulaire/assets/script.js
@@ -4,3 +4,6 @@ function afficher1() {
     let total = `${nom} ${prenom}`
     document.getElementById("result").value = total;
 }
+
+// autre soluce avec des name="" : f[2].onclick = function ()
+            // {f.nom_prenom.value = f[0].value +" "+ f[1].value}
